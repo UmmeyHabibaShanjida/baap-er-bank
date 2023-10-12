cda54d3f5dbf1dcc4b7d9c7085571a454dfc271c
UmmeyHabibaShanjida <uhshanjida@gmail.com>
2023-10-12 23:14:38 +0600

    added condition

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,20 +3,34 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithDrawAmount = parseFloat(newWithdrawAmountString);
+
+    withdrawField.value = '';
+
+    if(isNaN(newWithDrawAmount)){
+      alert('Please provide a valid number');
+      return;
+    }
     
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
-      withdrawTotalElement.innerText = currentWithdrawTotal;
+    
 
       const balanceTotalElement = document.getElementById('balance-total');
       const previousBalanceTotalString = balanceTotalElement.innerText;
       const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+      if(newWithDrawAmount > previousBalanceTotal){
+        alert('Baap er bank e tk nai');
+        return;
+      }
+
+      const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
+      withdrawTotalElement.innerText = currentWithdrawTotal;
+
       const newBalanceTotal  = previousBalanceTotal - newWithDrawAmount;
       balanceTotalElement.innerText = newBalanceTotal;
 
-      withdrawField.value = '';
+     
 })
